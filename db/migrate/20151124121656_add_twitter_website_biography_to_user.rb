2c850f74554e64d6a177fbf29c065c3237eaba6f
scarfdestroyer <bradyrudesill@gmail.com>
2015-11-24 10:37:51 -0800

    change user twitter, website, and biography from string to text

--- db/migrate/20151124121656_add_twitter_website_biography_to_user.rb
+++ db/migrate/20151124121656_add_twitter_website_biography_to_user.rb
@@ -1,7 +1,7 @@
 class AddTwitterWebsiteBiographyToUser < ActiveRecord::Migration
   def change
-    add_column(:users, :website, :string)
-    add_column(:users, :twitter, :string)
-    add_column(:users, :biography, :string)
+    add_column(:users, :website, :text)
+    add_column(:users, :twitter, :text)
+    add_column(:users, :biography, :text)
   end
 end
