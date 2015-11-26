d671dd289940080f44ebedd43a79a732a6d44d72
Nikola Begedin <begedinnikola@gmail.com>
2015-11-26 11:47:11 +0100

    Reformat and restructure specs into contexts

--- spec/requests/api/user_skills_spec.rb
+++ spec/requests/api/user_skills_spec.rb
@@ -6,16 +6,27 @@ describe "UserSkills API" do
 
   describe "POST /user_skills" do
 
-    it "requires authentication" do
-      post "#{host}/user_skills", { data: { } }
-      expect(last_response.status).to eq 401
-      expect(json).to be_a_valid_json_api_error.with_id "NOT_AUTHORIZED"
+    context "when unauthenticated" do
+      it "responds with a 401" do
+        post "#{host}/user_skills", { data: { } }
+        expect(last_response.status).to eq 401
+        expect(json).to be_a_valid_json_api_error.with_id "NOT_AUTHORIZED"
+      end
     end
 
-    it "sets user to current user"
-    it "sets skill to provided skill"
-    it "can create a new skill"
-    it "cannot create a duplicate user_skill"
+    context "when authenticated" do
+      context "when creation is succesful" do
+        it "responds with the created user_skill"
+        it "sets user to current user"
+        it "sets skill to provided skill"
+      end
+
+      context "when there's a user_skill with that pair of user_id and skill_id already" do
+        it "cannot create a duplicate user_skill"
+      end
+
+      it "can create a new skill"
+    end
   end
 
   describe "DELETE /user_skills/:id" do
@@ -23,15 +34,23 @@ describe "UserSkills API" do
       create(:user_skill, id: 1)
     end
 
-    it "requires authentication" do
-      delete "#{host}/user_skills/1"
+    context "when unauthenticated" do
+      it "responds with a 401" do
+        delete "#{host}/user_skills/1"
 
-      expect(last_response.status).to eq 401
-      expect(json).to be_a_valid_json_api_error.with_id "NOT_AUTHORIZED"
+        expect(last_response.status).to eq 401
+        expect(json).to be_a_valid_json_api_error.with_id "NOT_AUTHORIZED"
+      end
     end
 
-    it "requires the user to be the current user"
-    it "deletes the user_skill"
-    it "leaves user and skill untouched"
+    context "when authenticated" do
+
+      it "requires the user to be the current user"
+
+      context "when deletion is successful" do
+        it "deletes the user_skill"
+        it "leaves user and skill untouched"
+      end
+    end
   end
 end
