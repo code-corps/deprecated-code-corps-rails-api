7c1006f4100eaf85530671fc861b886573c0bae9
Nikola Begedin <begedinnikola@gmail.com>
2015-11-26 13:09:34 +0100

    Skill validations

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,6 @@
 class Skill < ActiveRecord::Base
   belongs_to :category
+
+  validates_presence_of :title
+  validates_presence_of :category
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -10,4 +10,9 @@ describe Skill, type: :model do
   describe "relationships" do
     it { should belong_to(:category) }
   end
+
+  describe "validations" do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:category) }
+  end
 end
