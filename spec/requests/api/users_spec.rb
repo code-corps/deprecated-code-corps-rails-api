0b61f74b5cf3367f9f09016767a693623e8e0625
Nikola Begedin <begedinnikola@gmail.com>
2015-11-26 09:19:19 +0100

    Failing specs

--- spec/requests/api/users_spec.rb
+++ spec/requests/api/users_spec.rb
@@ -160,7 +160,8 @@ describe "Users API" do
       post "#{host}/users/forgot_password", json_api_params
 
       expect(last_response.status).to eq 422
-      expect(json.errors.email).to include "doesn't exist"
+      expect(json).to be_a_valid_json_api_error.with_id "VALIDATION_ERROR"
+      expect(json).to contain_an_error_of_type("VALIDATION_ERROR").with_message "User doesn't exist"
     end
   end
 
@@ -200,7 +201,8 @@ describe "Users API" do
       post "#{host}/users/reset_password", json_api_params
 
       expect(last_response.status).to eq 422
-      expect(json.errors.password).to include "couldn't be reset"
+      expect(json).to be_a_valid_json_api_error.with_id "VALIDATION_ERROR"
+      expect(json).to contain_an_error_of_type("VALIDATION_ERROR").with_message "Password couldn't be reset"
     end
   end
 
