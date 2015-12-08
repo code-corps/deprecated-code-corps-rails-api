32e3e6123a5b25049d1cd2ac6de74023819c69bc
scarfdestroyer <bradyrudesill@gmail.com>
2015-12-08 15:56:46 -0800

    add scaffolding for project policy

---
+++ spec/policies/project_policy_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+describe ProjectPolicy do
+
+  subject { described_class }
+
+
+  before do
+
+  end
+
+  permissions :index? do
+
+  end
+
+  permissions :show? do
+
+  end
+
+  permissions :create? do
+
+  end
+
+  permissions :delete? do
+
+  end
+end
