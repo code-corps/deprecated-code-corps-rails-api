86f9e83cebf903bf0db662fc452bbe121e4037a3
Josh Smith <joshdotsmith@gmail.com>
2015-12-11 14:11:13 -0800

    Add worker and worker specs

--- spec/lib/code_corps/scenario/notify_pusher_of_post_image_spec.rb
+++ spec/lib/code_corps/scenario/notify_pusher_of_post_image_spec.rb
@@ -9,7 +9,7 @@ module CodeCorps
         file = File.open("#{Rails.root}/spec/sample_data/base64_images/gif.txt", 'r')
         open(file) { |io| io.read }
       }
-      let(:post_image) { create(:post_image, :with_s3_image, filename: "default-avatar.png", base64_photo_data: gif_string) }
+      let(:post_image) { create(:post_image, :with_s3_image, filename: "jake.gif", base64_photo_data: gif_string) }
 
       describe "#call" do
 
@@ -20,7 +20,7 @@ module CodeCorps
             filename: post_image.filename,
             url: post_image.image.url
           }
-          
+
           expect_any_instance_of(Pusher::Client).to receive(:trigger).with(
             "private-user-#{post_image.user.id}",
             'post_image_uploaded',
---
+++ spec/workers/notify_pusher_of_post_image_worker_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe NotifyPusherOfPostImageWorker do
+
+  let(:gif_string) {
+    file = File.open("#{Rails.root}/spec/sample_data/base64_images/gif.txt", 'r')
+    open(file) { |io| io.read }
+  }
+  let(:post_image) { create(:post_image, :with_s3_image, filename: "jake.gif", base64_photo_data: gif_string) }
+
+  it 'adds a profile picture from facebook' do
+    expect_any_instance_of(CodeCorps::Scenario::NotifyPusherOfPostImage).to receive(:call).exactly(1).times
+    NotifyPusherOfPostImageWorker.new.perform(post_image.id)
+  end
+end
