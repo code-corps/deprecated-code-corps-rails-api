34aca544e37af811d7e990b9886bab9ec60383c9
Justin Abrahms <justin@abrah.ms>
2016-01-01 13:31:38 -0500

    '&&' not 'and'

--- spec/utils.rb
+++ spec/utils.rb
@@ -1,6 +1,6 @@
 FB_ENABLED = "Facebook environment variables not set" unless
-  ENV["FACEBOOK_APP_ID"] and
-  ENV["FACEBOOK_APP_SECRET"] and
+  ENV["FACEBOOK_APP_ID"] &&
+  ENV["FACEBOOK_APP_SECRET"] &&
   ENV["FACEBOOK_REDIRECT_URL"]
 CLOUDFRONT_ENABLED = "Cloudfront ENV variables not set" unless
   ENV["CLOUDFRONT_DOMAIN"]
