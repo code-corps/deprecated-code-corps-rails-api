e03383b3f876a9f62dbf6fb5c7f07ed4e8ec1d2d
Nikola Begedin <begedinnikola@gmail.com>
2016-01-18 15:11:51 +0100

    Fixed n+1 queries for PostPolicy

--- app/policies/post_policy.rb
+++ app/policies/post_policy.rb
@@ -51,12 +51,16 @@ class PostPolicy
   private
 
     def organization_member_for_user
-      @organization_member_for_user ||= OrganizationMembership.find_by(member: user, organization: post.project.organization)
+      @organization_member_for_user ||= OrganizationMembership.find_by(
+        member_id: user.id, organization_id: post.project.organization_id)
     end
 
     def current_user_is_at_least_contributor_to_organization?
       return false unless organization_member_for_user
-      return true if organization_member_for_user.contributor? or organization_member_for_user.admin? or organization_member_for_user.owner?
+      return true if (
+        organization_member_for_user.contributor? or
+        organization_member_for_user.admin? or
+        organization_member_for_user.owner?)
     end
 
     def current_user_is_at_least_admin_in_organization?
