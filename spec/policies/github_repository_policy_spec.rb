950c7a917c59ddf6d26f15f04a2e39f50e9325a3
Nikola Begedin <begedinnikola@gmail.com>
2016-01-18 09:17:43 +0100

    Fixed GithubRepositoryPolicy spec

--- spec/policies/github_repository_policy_spec.rb
+++ spec/policies/github_repository_policy_spec.rb
@@ -5,47 +5,48 @@ describe GithubRepositoryPolicy do
   subject { described_class }
 
   before do
-    @project = create(:project)
+    organization = create(:organization)
+    project = create(:project, organization: organization)
 
-    @author = create(:user)
-    @other_user = create(:user)
+    @github_repository = create(:github_repository, project: project)
 
-    @comment = create(:comment, user: @author)
-  end
+    @unassociated_user = create(:user)
 
-  permissions :index? do
-    it "is permited for anyone" do
-      expect(subject).to permit(nil, Comment)
-    end
-  end
+    @pending_user = create(:user)
+    create(:organization_membership, member: @pending_user, organization: organization, role: :pending)
 
-  permissions :show? do
-    it "is permited for anyone" do
-      expect(subject).to permit(nil, Comment)
-    end
+    @contributor = create(:user)
+    create(:organization_membership, member: @contributor, organization: organization, role: :contributor)
+
+    @admin = create(:user)
+    create(:organization_membership, member: @admin, organization: organization, role: :admin)
+
+    @owner = create(:user)
+    create(:organization_membership, member: @owner, organization: organization, role: :owner)
   end
 
   permissions :create? do
     it "is not permitted for unauthenticated users" do
-      expect(subject).not_to permit(nil, Comment)
+      expect(subject).not_to permit(nil, @github_repository)
     end
 
-    it "is permitted for authenticated users" do
-      expect(subject).to permit(@other_user, Comment)
+    it "is not permitted for users not associated with the organization" do
+      expect(subject).not_to permit(@unassociated_user, @github_repository)
     end
-  end
-
-  permissions :update? do
-    it "is not permitted for unauthenticated users" do
-      expect(subject).not_to permit(nil, Comment)
+    it "is not permitted for users pending for organization membership" do
+      expect(subject).not_to permit(@pending_user, @github_repository)
     end
 
-    it "is not permitted for authenticated users who did not create the comment" do
-      expect(subject).not_to permit(@other_user, @comment)
+    it "is not permitted for organization contributors" do
+      expect(subject).not_to permit(@contributor, @github_repository)
     end
 
-    it "is permitted for authenticated users who did create the comment" do
-      expect(subject).to permit(@author, @comment)
+    it "is permitted for organization admins" do
+      expect(subject).to permit(@admin, @github_repository)
+    end
+
+    it "is permitted for organization onwers" do
+      expect(subject).to permit(@owner, @github_repository)
     end
   end
 end
