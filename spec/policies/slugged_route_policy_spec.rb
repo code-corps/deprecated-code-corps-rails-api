e4c2042460e3616bd2bc35c195e4c6351b14477e
Nikola Begedin <begedinnikola@gmail.com>
2016-01-18 10:54:00 +0100

    Add slugged route policy

---
+++ spec/policies/slugged_route_policy_spec.rb
@@ -0,0 +1,16 @@
+require 'rails_helper'
+
+describe SluggedRoutePolicy do
+  subject { described_class }
+
+  before do
+    organization = create(:organization)
+    @slugged_route = organization.slugged_route
+  end
+
+  permissions :show? do
+    it "is permited for anyone" do
+      expect(subject).to permit(nil, @slugged_route)
+    end
+  end
+end
