b3d10174183db322da131b1406961f5d272da7c3
Nikola Begedin <begedinnikola@gmail.com>
2016-01-19 10:30:28 +0100

    Seeds for a basic organization, project and project posts

---
+++ db/fixtures/organizations.rb
@@ -0,0 +1,5 @@
+Organization.seed do |organization|
+  organization.id   = 1
+  organization.name = "Code Corps"
+  organization.slug = "code_corps"
+end
---
+++ db/fixtures/projects.rb
@@ -0,0 +1,7 @@
+Project.seed do |project|
+  project.id              = 1
+  project.title           = "Code Corps"
+  project.description     = "A basic project for use in development"
+  project.organization_id = 1
+  project.slug            = "code_corps"
+end
