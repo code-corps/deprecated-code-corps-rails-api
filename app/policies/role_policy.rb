6f212647d5008b2d20706b339b434068334e031d
Josh Smith <joshdotsmith@gmail.com>
2016-05-18 14:24:39 -0700

    Add RolePolicy

--- app/policies/role_policy.rb
+++ app/policies/role_policy.rb
@@ -1,10 +1,20 @@
 class RolePolicy
-  def initialize (user, role)
+  def initialize(user, role)
     @user = user
     @role = role
   end
 
   def index?
     true
+  end
+
+  def create?
+    return unless @user.present?
+    @user.admin?
+  end
+
+  def update?
+    return unless @user.present?
+    @user.admin?
   end
 end
