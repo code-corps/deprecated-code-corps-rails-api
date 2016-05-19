f12e0f3f0b53a2d4c77fb12f6a6df43c58f5ec7c
Josh Smith <joshdotsmith@gmail.com>
2016-05-19 12:10:18 -0700

    Fix hound

--- app/policies/project_category_policy.rb
+++ app/policies/project_category_policy.rb
@@ -21,16 +21,22 @@ class ProjectCategoryPolicy
   private
 
     def organization_member_for_user
-      @organization_member_for_user ||= OrganizationMembership.find_by(member: user, organization: project_category.project.organization)
+      @organization_member_for_user ||=
+        OrganizationMembership.find_by(
+          member: user, organization: project_category.project.organization
+        )
     end
 
     def current_user_is_at_least_contributor_to_organization?
       return false unless organization_member_for_user
-      return true if organization_member_for_user.contributor? or organization_member_for_user.admin? or organization_member_for_user.owner?
+      return true if organization_member_for_user.contributor? ||
+                     organization_member_for_user.admin? ||
+                     organization_member_for_user.owner?
     end
 
     def current_user_is_at_least_admin_in_organization?
       return false unless organization_member_for_user
-      return true if organization_member_for_user.admin? or organization_member_for_user.owner?
+      return true if organization_member_for_user.admin? ||
+                     organization_member_for_user.owner?
     end
 end
