b156185b0e6e988794ac16741a5cba6143fa3432
Josh Smith <joshdotsmith@gmail.com>
2016-05-19 20:41:29 -0700

    Change to required true

--- app/models/project_category.rb
+++ app/models/project_category.rb
@@ -1,8 +1,6 @@
 class ProjectCategory < ActiveRecord::Base
-  belongs_to :project
-  belongs_to :category
+  belongs_to :project, required: true
+  belongs_to :category, required: true
 
-  validates_presence_of :project
-  validates_presence_of :category
   validates_uniqueness_of :project_id, scope: :category_id
 end
