73949904b8a5e8d5f2980121ed3f2d94d8bcb87e
Nikola Begedin <begedinnikola@gmail.com>
2016-06-22 11:59:54 +0200

    Removed _preview fields from post fixtures

--- db/fixtures/004_posts.rb
+++ db/fixtures/004_posts.rb
@@ -2,9 +2,7 @@
   Post.seed do |post|
     post.title = "A task"
     post.body = "This is a basic task"
-    post.body_preview = "This is a basic task"
     post.markdown = "This is a basic task"
-    post.markdown_preview = "This is a basic task"
     post.project_id = 1
     post.user_id = 2
     post.post_type = "task"
@@ -14,9 +12,7 @@
   Post.seed do |post|
     post.title = "An idea"
     post.body = "This is a basic idea"
-    post.body_preview = "This is a basic idea"
     post.markdown = "This is a basic idea"
-    post.markdown_preview = "This is a basic idea"
     post.project_id = 1
     post.user_id = 2
     post.post_type = "idea"
@@ -26,9 +22,7 @@
   Post.seed do |post|
     post.title = "An issue"
     post.body = "This is a basic issue"
-    post.body_preview = "This is a basic issue"
     post.markdown = "This is a basic progress post"
-    post.markdown_preview = "This is a basic progress post"
     post.project_id = 1
     post.user_id = 2
     post.post_type = "issue"
